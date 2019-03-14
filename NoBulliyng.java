88f7bc03f72a9db57cee57ea0835b9ddc80935ad
NATALIA FERREIRA SOUSA <591182348@alunos.etec.itaqua>
2019-03-14 20:23:40 -0300

    Passo 4

--- NoBulliyng.java
+++ NoBulliyng.java
@@ -2,4 +2,12 @@ oi
 puclic class NoBulliyng {
 	public static void main(String[] args){
   }	  System.out.println('Programa NoBulliyng');
+	  System.out.println('Passo 1: Observar em qual brincadeira meu colega se sente mal.');
+	  System.out.println('Passo 2: A pessoa se comunicar, falando que se sente mal com a brincadeira.');
+	  System.out.println('Passo 3: Parar com a brincadeira na qual a pessoa se sente mal.');
+	  System.out.println('Passo 4: Observar se a pessoa precisa de algum tipo de ajuda.');
+
+
+
+
 }
