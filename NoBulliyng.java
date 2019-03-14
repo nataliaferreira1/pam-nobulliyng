a357ea1f61aa3314d54b8bae4357274458ae8b20
NATALIA FERREIRA SOUSA <591182348@alunos.etec.itaqua>
2019-03-14 20:25:47 -0300

    Passo 5

--- NoBulliyng.java
+++ NoBulliyng.java
@@ -6,6 +6,7 @@ puclic class NoBulliyng {
 	  System.out.println('Passo 2: A pessoa se comunicar, falando que se sente mal com a brincadeira.');
 	  System.out.println('Passo 3: Parar com a brincadeira na qual a pessoa se sente mal.');
 	  System.out.println('Passo 4: Observar se a pessoa precisa de algum tipo de ajuda.');
+	  System.out.println('Passo 5: Deixar a pessoa desabafar com você, sem diminuir seus problemas.');
 
 
 
