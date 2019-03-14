20e097acc9b7e0a55861dc11a20b0f2ff111ff75
NATALIA FERREIRA SOUSA <591182348@alunos.etec.itaqua>
2019-03-14 20:49:59 -0300

    Passo 9

--- NoBulliyng.java
+++ NoBulliyng.java
@@ -1,15 +1,16 @@
 oi
 puclic class NoBulliyng {
 	public static void main(String[] args){
-  }	  System.out.println('Programa NoBulliyng');
-	  System.out.println('Passo 1: Observar em qual brincadeira meu colega se sente mal.');
-	  System.out.println('Passo 2: A pessoa se comunicar, falando que se sente mal com a brincadeira.');
-	  System.out.println('Passo 3: Parar com a brincadeira na qual a pessoa se sente mal.');
-	  System.out.println('Passo 4: Observar se a pessoa precisa de algum tipo de ajuda.');
-	  System.out.println('Passo 5: Deixar a pessoa desabafar com você, sem diminuir seus problemas.');
-	  System.out.println('Passo 6: Ajuda-lá com o que estiver ao seu alcance.);
-	  System.out.println('Passo 7: Se não poder ajudar ela com conselhos, orientar ela procurar um psicólogo.');
-	  System.out.println('Passo 8: Acompanhamento a distancia da pessoa.');
+  }	  System.out.println("Programa NoBulliyng");
+	  System.out.println("Passo 1: Observar em qual brincadeira meu colega se sente mal.");
+	  System.out.println("Passo 2: A pessoa se comunicar, falando que se sente mal com a brincadeira.");
+	  System.out.println("Passo 3: Parar com a brincadeira na qual a pessoa se sente mal.");
+	  System.out.println("Passo 4: Observar se a pessoa precisa de algum tipo de ajuda.");
+	  System.out.println("Passo 5: Deixar a pessoa desabafar com você, sem diminuir seus problemas.");
+	  System.out.println("Passo 6: Ajuda-lá com o que estiver ao seu alcance.");
+	  System.out.println("Passo 7: Se não poder ajudar ela com conselhos, orientar ela procurar um psicólogo.");
+	  System.out.println("Passo 8: Acompanhamento a distancia da pessoa.");
+	  System.out.println("Passo 9: Continuar ajudando ela com desabafos, sabendo que ela pode confiar em você.");
 
 
 
