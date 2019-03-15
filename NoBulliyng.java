c46cc8416e8a321cdf989c07f446d8275a4c3cd6
NATALIA FERREIRA SOUSA <591182348@alunos.etec.itaqua>
2019-03-14 21:30:17 -0300

    Finalizado

--- NoBulliyng.java
+++ NoBulliyng.java
@@ -1,7 +1,6 @@
-oi
-puclic class NoBulliyng {
+public class NoBulliyng {
 	public static void main(String[] args){
-  }	  System.out.println("Programa NoBulliyng");
+  	  System.out.println("Programa NoBulliyng");
 	  System.out.println("Passo 1: Observar em qual brincadeira meu colega se sente mal.");
 	  System.out.println("Passo 2: A pessoa se comunicar, falando que se sente mal com a brincadeira.");
 	  System.out.println("Passo 3: Parar com a brincadeira na qual a pessoa se sente mal.");
@@ -13,6 +12,6 @@ puclic class NoBulliyng {
 	  System.out.println("Passo 9: Continuar ajudando ela com desabafos, sabendo que ela pode confiar em você.");
 	  System.out.println("Passo 10: Elogiar pequenas atitudes.");
 
-
+}
 
 }
